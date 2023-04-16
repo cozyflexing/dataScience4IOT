30561a3f9b2c3386e5669e1e3804a078dda32ea1
cozy <95256892+cozyflexing@users.noreply.github.com>
2023-04-16 20:47:56 +0200

    Dash App

--- thingSpeakSenseHat.py
+++ thingSpeakSenseHat.py
@@ -1,108 +1,78 @@
-import time
-import requests
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 from sense_hat import SenseHat
-from time import sleep
+import requests
 
-sense = SenseHat() # Hier initialiseren je de SenseHat library.
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# Kleuren voor de nachtmodus.
-nightRed = (64, 0, 0)
-nightGreen = (0, 64, 0)
-nightOrange = (255, 140, 0)
-nightWhite = (64, 64, 64)
+sense = SenseHat()
 
-# Kleuren voor de dagmodus.
-red = (255, 0, 0)
-green = (0, 128, 0)
-orange = (255, 100, 0)
-white = (255, 255, 255)
-
-writeAPIkey = "" # Vul hier je eigen writeAPIkey in. 
-channelID = "" # Vul hier je eigen channelID in.
-
-
-url= "https://api.thingspeak.com/update"
-
-# Geeft een float temperatuur terug.
-def getTemperature():
-    temperature = sense.get_temperature_from_pressure() - 7.3 
-    # De '-7.3' is voor juiste calibratie, de temperatuur sensor geeft een te hooge waarde, 
-    # dus die heb ik aangepast aan de hand van een preciezere temperatuur sensor. 
+def get_temperature():
+    temperature = sense.get_temperature_from_pressure() - 7.3
     return temperature
 
-# Geeft een float luchtdruk terug.
-def getPressure():
-        pressure = sense.get_pressure()
-        return pressure
+def get_pressure():
+    pressure = sense.get_pressure()
+    return pressure
 
-# Geeft een float luchtvochtigheid terug. 
-def getHumidity():
+def get_humidity():
     humidity = sense.get_humidity()
     return humidity
 
-# Geeft een dictionary met daarin de juiste waardes voor de verschillende fields op het Thingspeak dashboard.
-def updateQuery(writeAPIkey,temperature, pressure, humidity):
-    queries = {"api_key": writeAPIkey,
-                "field1": temperature,
-                "field2": pressure,
-                "field3": humidity}
-    return queries
+def show_message(message, text_colour, background_colour):
+    sense.show_message(message, text_colour=text_colour, back_colour=background_colour, scroll_speed=0.15)
 
-# Deze functie stuurt de dictionary naar de update url om zo het dashboard te updaten.
-def updateAPI(url):
-    requests.get(url, params=updateQuery(writeAPIkey, getTemperature(), getPressure(), getHumidity()))
+app.layout = dbc.Container([
+    html.H1("SenseHat Weather Monitor", style={"text-align": "center"}),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            html.H4("Temperature"),
+            html.P(id="temperature")
+        ]),
+        dbc.Col([
+            html.H4("Pressure"),
+            html.P(id="pressure")
+        ]),
+        dbc.Col([
+            html.H4("Humidity"),
+            html.P(id="humidity")
+        ])
+    ]),
+    dbc.Button("Show on LED Matrix", id="show-button", color="primary", className="mr-1"),
+    dcc.Interval(id="interval", interval=5*60*1000, n_intervals=0)
+])
 
-# Geeft een integer terug die de uur van de dag heeft als waarde.
-def getLocalTimeHour():
-    currentHour = time.localtime()
-    return currentHour[3]
+@app.callback(
+    Output("temperature", "children"),
+    Output("pressure", "children"),
+    Output("humidity", "children"),
+    Input("interval", "n_intervals")
+)
+def update_values(n):
+    temperature = f"{round(get_temperature(), 2)} °C"
+    pressure = f"{round(get_pressure(), 2)} hPa"
+    humidity = f"{round(get_humidity(), 2)} %"
+    return temperature, pressure, humidity
 
-# Verlaagd felheid van ledmatrix.
-def setLowLight():
-    sense.low_light = True
+@app.callback(
+    Output("show-button", "n_clicks"),
+    Input("show-button", "n_clicks")
+)
+def display_on_led_matrix(n_clicks):
+    if n_clicks is not None and n_clicks > 0:
+        temp = round(get_temperature(), 2)
+        message = f"{temp} C"
+        if temp >= 20:
+            show_message(message, (255, 255, 255), (0, 128, 0))
+        elif 18 <= temp < 20:
+            show_message(message, (255, 255, 255), (255, 100, 0))
+        else:
+            show_message(message, (255, 255, 255), (255, 0, 0))
+    return 0
 
-# Verhoogd felheid van ledmatrix.
-def setBrightLight():
-    sense.low_light = False
-
-# Laat informatie zien op de ledmatrix.
-def showMessage(message, textColour, backgroundColour):
-    sense.show_message(message, text_colour=textColour, back_colour=backgroundColour, scroll_speed=0.15)
-
-# Start infinite loop.
-while True:
-    if getLocalTimeHour() < 7 or getLocalTimeHour() > 20: # We checken hier hoe laat het is zodat we dagmodus of nachtmodus kunnen activeren.
-        setLowLight() # Nachtmodus aan.
-        if getTemperature() >= 20: # Temperatuur check.
-            showMessage(f"{round(getTemperature(),2)} C", nightWhite, nightGreen)
-            updateAPI(url)
-            sense.clear() # Zet scherm uit.
-            sleep(300) # Wacht 5 minuten voordat het scherm weer aan kan.
-            
-        if 18 <= getTemperature() < 20: # Temperatuur check.
-            showMessage(f"{round(getTemperature(),2)} C", nightWhite, nightOrange)
-            updateAPI(url)
-            sense.clear() # Zet scherm uit.
-            sleep(300) # Wacht 5 minuten voordat het scherm weer aan kan.
-            
-        if 0 < getTemperature() < 18: # Temperatuur check.
-            showMessage(f"{round(getTemperature(),2)} C", nightWhite, nightRed)
-            updateAPI(url)
-            sense.clear() # Zet scherm uit.
-            sleep(300) # Wacht 5 minuten voordat het scherm weer aan kan.
-            
-    else:
-        setBrightLight() # Dagmodus aan.
-        if getTemperature() >= 20: # Temperatuur check.
-            showMessage(f"{round(getTemperature(),2)} C", white, green)
-            updateAPI(url)
-            
-        if 18 <= getTemperature() < 20: # Temperatuur check.
-            showMessage(f"{round(getTemperature(),2)} C", white, orange)
-            updateAPI(url)
-            
-        if 0 < getTemperature() < 18: # Temperatuur check.
-            showMessage(f"{round(getTemperature(),2)} C", white, red)
-            updateAPI(url)
-            
-        
+if __name__ == "__main__":
+    app.run_server(debug=True)
